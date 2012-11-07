66c72382b769910628061addb94db0d876393bd4
Karell Ste-Marie <karell.stemarie@gmail.com>
2012-11-07 16:27:50 -0500

    Readded the MassTransit basics

---
+++ src/Extensions/SimpleCqrs.MassTransit/ConfigSimpleCqrs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MassTransit;
+using MassTransit.BusConfigurators;
+using SimpleCqrs.Commanding;
+
+namespace SimpleCqrs.MassTransit
+{
+    public class ConfigSimpleCqrs
+    {
+        private readonly ISimpleCqrsRuntime _runtime;
+        public IServiceLocator ServiceLocator { get; private set; }
+
+        public ConfigSimpleCqrs(ISimpleCqrsRuntime runtime)
+        {
+            _runtime = runtime;
+        }
+
+        public void Configure(Action<ServiceBusConfigurator> config)
+        {
+            var bus = ServiceBusFactory.New(config);
+
+            _runtime.Start();
+
+            ServiceLocator = _runtime.ServiceLocator;
+            ServiceLocator.Register(bus);
+            ServiceLocator.Register(_runtime);
+        }
+
+        public ConfigSimpleCqrs UseMTCommandBus()
+        {
+            var config = new ConfigCommandBus(_runtime);
+            var commandBus = config.Configure(this);
+
+            ServiceLocator.Register<ICommandBus>(commandBus);
+
+            return this;
+        }
+    }
+}
---
+++ src/Extensions/SimpleCqrs.MassTransit/ConfigureSimpleCqrs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MassTransit.BusConfigurators;
+
+namespace SimpleCqrs.MassTransit
+{
+    public static class ConfigureSimpleCqrs
+    {
+        public static ConfigSimpleCqrs SimpleCqrs(this Action<ServiceBusConfigurator> configure, ISimpleCqrsRuntime runtime)
+        {
+            var configSimpleCqrs = new ConfigSimpleCqrs(runtime);
+            configSimpleCqrs.Configure(configure);
+            return configSimpleCqrs;
+        }
+    }
+}
